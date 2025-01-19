1f238c52e72095a8bfd064064d521e0218cc7fec
Sky Randy <soras1758@gmail.com>
2025-01-19 15:27:35 +0900

    Add homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,35 @@
 import styled from "styled-components";
-
+import heroImage from "../assets/homepage-hero.jpg";
 const Container = styled.div`
-  padding: 2em;
-  margin-top: 3em;
+  padding: 4em;
+  background-image: url(${heroImage});
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+  color: #ffffff;
+  h1 > span {
+    font-weight: 800;
+    font-size: 2em;
+  }
+`;
+const WelcomeMessage = styled.div`
+  width: min(1200px, 80%);
 `;
 const Home = () => {
   return (
     <Container>
-      <h1>Welcome to Home</h1>
+      <WelcomeMessage>
+        <h1>
+          Welcom to <br></br>
+          <span>The Awesome Shop</span>
+        </h1>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quam
+          reiciendis velit id quisquam incidunt quo. Obcaecati debitis minima
+          animi, incidunt, sint recusandae hic aperiam a illo distinctio, sit
+          esse?
+        </div>
+      </WelcomeMessage>
     </Container>
   );
 };
