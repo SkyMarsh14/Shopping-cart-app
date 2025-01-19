78f388a4dcd037505edeb36db88102397a1bdd53
Sky Randy <soras1758@gmail.com>
2025-01-19 13:33:16 +0900

    Add info of total price and item nubmers in the cart on checkout page

---
+++ src/components/TotalPrice.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { useContext } from "react";
+import CartContext from "../util/CartContext";
+
+const Container = styled.div`
+  height: 3em;
+  margin-left: auto;
+  padding-right: 1em;
+  padding-top: 0.5em;
+`;
+const Span = styled.span`
+  font-weight: 800;
+  font-size: 1.2em;
+  margin-left: 1em;
+`;
+const TotalPrice = () => {
+  const [cart] = useContext(CartContext);
+  const totalPrice = cart.reduce((accumulator, item) => {
+    return (accumulator += item.count * item.price);
+  }, 0);
+  const totalCount = cart.reduce((accumulator, item) => {
+    return (accumulator += item.count);
+  }, 0);
+  return (
+    <Container>
+      Subtotal ({totalCount} items) <Span>${totalPrice}</Span>
+    </Container>
+  );
+};
+
+export default TotalPrice;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,45 +1,65 @@
 import CartContext from "../util/CartContext";
 import styled from "styled-components";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import TotalPrice from "../components/TotalPrice";
 
 const Container = styled.div`
   padding: 2em;
   margin-top: 3em;
   background-color: #eaeded;
 `;
-const ItemCard = styled.div`
+const GridWrapper = styled.div`
   display: grid;
   background-color: white;
+  padding: 1em;
+  width: min(1200px, 80%);
+  margin: auto;
+  > div:not(:last-child) {
+    border-bottom: solid black 1px;
+  }
+`;
+const ItemCard = styled.div`
+  display: grid;
   grid-template-columns: 1fr 3fr 0.5fr;
   gap: 2em;
   margin-top: 2em;
   padding: 1.5em;
+  align-items: center;
 `;
 const H1 = styled.h1`
-  border-bottom: solid 1px black;
+  border-bottom: solid black 1px;
 `;
-const DetailsDiv = styled.div``;
+const DetailsDiv = styled.div`
+  :first-child {
+    font-size: 1.2em;
+    font-weight: 900;
+    margin-bottom: 0.5em;
+  }
+`;
 const Cart = () => {
   const [cart, setCart] = useContext(CartContext);
   return (
     <Container>
-      <H1>Shopping Cart</H1>
-      {cart ? (
-        cart.map((item) => {
-          return (
-            <ItemCard key={crypto.randomUUID()}>
-              <img src={item.image}></img>
-              <DetailsDiv>
-                <div>{item.title}</div>
-                <div>{item.description}</div>
-              </DetailsDiv>
-              <div>${item.price * item.count}</div>
-            </ItemCard>
-          );
-        })
-      ) : (
-        <div>No item in the cart</div>
-      )}
+      <GridWrapper>
+        <H1>Shopping Cart</H1>
+        {cart ? (
+          cart.map((item) => {
+            return (
+              <ItemCard key={crypto.randomUUID()}>
+                <img src={item.image}></img>
+                <DetailsDiv>
+                  <div>{item.title}</div>
+                  <div>{item.description}</div>
+                </DetailsDiv>
+                <div>${item.price * item.count}</div>
+              </ItemCard>
+            );
+          })
+        ) : (
+          <div>No item in the cart</div>
+        )}
+        {cart && <TotalPrice></TotalPrice>}
+      </GridWrapper>
     </Container>
   );
 };
